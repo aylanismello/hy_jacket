b5d60a3c95b9820c092a5116da62ce1e7dea132a
Aylan Mello <aylanismello@gmail.com>
2017-10-27 19:39:44 -0700

    audio now being received and written 2 file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const ss = require('socket.io-stream');
+const path = require('path');
+const fs = require('fs')
+
 const PORT = process.env.NODE_ENV === 'production' ? 80 : 3000;
 
 app.get('/', (req, res) => {
   res.send('yo');
 })
 
+// const io = require('socket.io').listen(80);
+
+// io.on('connection', function(socket) {
+//   ss(socket).on('audio', function(stream, data) {
+//     const filename = path.basename(data.name);
+//     stream.pipe(fs.createWriteStream(filename));
+//   });
+// });
+
 io.on('connection', (socket) => {
    console.log('connected to server');
+
+  // for audio
+  ss(socket).on('audio', function(stream, data) {
+    console.log('got audio');
+    // console.dir(fs);
+    stream.pipe(fs.createWriteStream('./yo.wav'));
+    // const filename = path.basename(data.name);
+    // stream.pipe(fs.createWriteStream(filename));
+  });
 
   socket.on('tick', (from, msg) => {
     console.log('MSG from', from, ' saying ', msg);
