00dacb556539bebb339783f7ba5a8e611bcb1d14
Aylan Mello <aylanismello@gmail.com>
2017-10-28 13:27:26 -0700

    wont break when client reconnects

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const fs = require('fs')
 
 const PORT = process.env.NODE_ENV === 'production' ? 80 : 3000;
-const relayedAudioStream = ss.createStream();
 
 app.get('/', (req, res) => {
   res.send('yo');
@@ -16,6 +15,7 @@ io.on('connection', (socket) => {
    console.log('connected to server');
   // for audio
   ss(socket).on('audio', function(audioStream, data) {
+    const relayedAudioStream = ss.createStream();
     console.log('got audio audioStream, piping stream to clients');
     ss(io.sockets).emit('audio', relayedAudioStream, { name: 'whatever'})
     // ss(socket).emit('audio', relayedAudioStream, { name: 'whatever'})
